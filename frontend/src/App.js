b974e8be8cef98bbc65970a721b82a61e2fcef99
sharryls <sharryls55@gmail.com>
2022-06-21 00:21:21 +0800

    navbar (without lang and currency)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,10 @@ import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
 import Booking from "./pages/booking/Booking";
 
+import Register from "./pages/proxy/Register";
+import Login from "./pages/proxy/Login";
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -16,7 +20,10 @@ function App() {
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
         <Route path="/booking" element={<Booking/>}/>
+
         
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/Login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,17 +1,33 @@
 import "./navbar.css"
+import { Routes, Route, useNavigate } from "react-router-dom"
+import Register from "../../pages/proxy/Register"
+import Login from "../../pages/proxy/Login"
 
-const Navbar = () => {
+export default function Navbar() {
+  const navigate = useNavigate()
+
+  const registerRoute = () => {
+    navigate("/register")
+  }
+  const loginRoute = () => {
+    navigate("/login")
+  }
+
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Ascenda booking</span>
+        <span className="logo">Ascenda Hotel Booking</span>
         <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
+          <button className="navButton" onClick={registerRoute}>Register</button>
+          <button className="navButton" onClick={loginRoute}>Login</button>
+
+          <Routes>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/Login" element={<Login/>}/>
+          </Routes>
         </div>
       </div>
     </div>
   )
+  
 }
-
-export default Navbar
