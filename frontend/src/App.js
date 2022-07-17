e9fada380e50cf0cf3bfc25b4240f0646799741f
Ilikefish01 <guykrittanat810@gmail.com>
2022-07-17 13:56:22 +0800

    Task 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Hotels from "./pages/Hotels";
-import Booking from "./pages/Booking";
+import Booking from "./pages/payment/Booking";
+import Pay from "./pages/payment/Pay";
+import Summary from "./pages/payment/Summary";
 import Hotel from "./pages/Hotel";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -19,6 +21,8 @@ function App() {
         <Route path="/hotels" element={<Hotels/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
         <Route path="/booking" element={<Booking/>}/>
+        <Route path="/pay" element={<Pay/>}/>
+        <Route path="/summary" element={<Summary/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/Login" element={<Login/>}/>
       </Routes>
@@ -28,3 +32,4 @@ function App() {
 
 
 export default App;
+
