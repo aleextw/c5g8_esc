70163ab943809d4deb6498af1b9f534f41197e1f
aleextw <lee.tze.wei.alex.joel@gmail.com>
2022-07-23 15:07:59 +0800

    -

--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { Component } from "react";
 import { getHotels } from "../api/services/destinations";
 import { Flex, Heading, Image, Stack, Text, Button, Box, Center, Spacer, Spinner, UnorderedList, ListItem, Show, StackDivider } from "@chakra-ui/react"
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function formatDistance(distance) {
     if (distance < 1000) {
@@ -27,7 +28,7 @@ function Card(props) {
         // navigate(`/hotel?hotel_uid=${props.uid}&dest_uid=${params.get("uid")}&checkInDate=${params.get("checkInDate")}&checkOutDate=${params.get("checkOutDate")}&guests=${params.get("guests")}&currency=SGD`);
     }
 
-    return (<Flex  name="HotelCard" onClick={searchHotel} cursor={"pointer"}>
+    return (<Flex  name="HotelCard" onClick={searchHotel} cursor={"pointer"} mt={5} mb={5} shadow={"base"}>
         <Image boxSize="150px" objectFit="cover" w="25%" src={props.image} />
         <Stack align="center" w="75%" direction={{ base: 'column', md: 'row' }} divider={<StackDivider borderColor='#F5F4F1' borderWidth="1px"/>}>
             <Stack p="2" direction="column" w={{base: "100%", md: "60%", sm:"60%"}}>
@@ -63,6 +64,7 @@ export default class CardList extends Component {
             reviewRange: [],
             priceRange: [],
             typeFilter: [],
+            items: [],
             params: props.params
         };
 
@@ -72,7 +74,21 @@ export default class CardList extends Component {
     setHotels(hotels) {
         // TODO: Add completed checking
         this.setState({hotels: hotels});
+        this.setState({items: hotels.hotels.slice(0,11)})
     }
+
+    fetchMoreData = () => {
+        // a fake async api call like which sends
+        // 20 more records in 1.5 secs
+        const cardLength = this.state.items.length;
+        console.log("fetching more data");
+        setTimeout(() => {
+          this.setState({
+            items: this.state.items.concat(this.state.hotels.hotels.slice(cardLength, cardLength+10))
+          });
+        }, 1500);
+        console.log("more data fetched, items: ", this.state.items);
+      };
 
     componentDidMount() {
         // TODO: Figure out why its triggering twice
@@ -99,27 +115,38 @@ export default class CardList extends Component {
         if (this.state.hotels.hotels.length > 0) {
             return (
                 <Box w="100%" h={{base: "80vh", lg:"80vh", md: "70vh", sm: "70vh"}}>
-                    <Stack w='100%' h='100%' overflowY='scroll' className="hotels-list" backgroundColor="white" divider={<StackDivider borderColor='#898989' borderWidth="1px"/>}>
-                    { this.state.hotels.hotels.slice(0, 10).map((hotel) => {
-                        return (
-                            <Card 
-                                searchRank={hotel["searchRank"]}
-                                price={hotel["price"]}
-                                points={hotel["points"]}
-                                latitude={hotel["latitude"]}
-                                longitude={hotel["longitude"]}
-                                distance={hotel["distance"]}
-                                name={hotel["name"]}
-                                address={hotel["address"]}
-                                rating={hotel["rating"]}
-                                review={hotel["review"]}
-                                image={hotel["photo"]}
-                                uid={hotel["uid"]}
-                            />      
-                        )
-                    })}
+                    <Stack id="card-stack" w='100%' h='100%' overflowY='scroll' className="hotels-list" backgroundColor="white" divider={<StackDivider borderColor='#898989' borderWidth="1px"/>}>
+                        <InfiniteScroll
+                        dataLength={this.state.items.length}
+                        next={this.fetchMoreData}
+                        hasMore={true}
+                        loader={<h4>Please Wait...</h4>}
+                        scrollableTarget="card-stack"
+                        >
+
+                            { this.state.items.map((hotel) => {
+                                return (
+                                    <Card 
+                                        searchRank={hotel["searchRank"]}
+                                        price={hotel["price"]}
+                                        points={hotel["points"]}
+                                        latitude={hotel["latitude"]}
+                                        longitude={hotel["longitude"]}
+                                        distance={hotel["distance"]}
+                                        name={hotel["name"]}
+                                        address={hotel["address"]}
+                                        rating={hotel["rating"]}
+                                        review={hotel["review"]}
+                                        image={hotel["photo"]}
+                                        uid={hotel["uid"]}
+                                    />      
+                                )
+                            })}
+
+                        </InfiniteScroll>
                     </Stack>
                 </Box>);
+
         } else if (this.state.hotels.completed === false) {
             return (<Box w="100%" h="80vh">
                 <Center w="100%" h="100%">
